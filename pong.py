a441a99672cb84b9333afd0a515e57ae3d771b72
ericatsu <ekaatsu@st.ug.edu.gh>
2022-01-12 01:24:20 +0000

    moving paddles with s & w keys

--- pong.py
+++ pong.py
@@ -28,6 +28,31 @@ paddle_b.penup()
 paddle_b.goto(350, 0)
 
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape('square')
+ball.color('white')
+ball.penup()
+ball.goto(0, 0)
+
+
+# Functions to move paddles
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+# Keyboard binding
+wnd.listen()
+wnd.onkeypress(paddle_a_up, 'w')
+wnd.onkeypress(paddle_a_down, 's')
+
 
 #Main Game Loop
 while True:
