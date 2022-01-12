f5bff36d49085613e074aff93e84e354d3568153
ericatsu <ekaatsu@st.ug.edu.gh>
2022-01-12 15:39:41 +0000

    paddle and ball

--- pong.py
+++ pong.py
@@ -96,3 +96,12 @@ while True:
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+
+# paddle and the ball collection
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor -40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor - 40):
+        ball.setx(-340)
+        ball.dx *= -1
