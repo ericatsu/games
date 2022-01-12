0028233928799c8e52a74aba6be8dc4d6f5085ae
ericatsu <ekaatsu@st.ug.edu.gh>
2022-01-12 14:40:39 +0000

    Moving the ball

--- pong.py
+++ pong.py
@@ -34,6 +34,9 @@ ball.shape('square')
 ball.color('white')
 ball.penup()
 ball.goto(0, 0)
+# Pixels the ball move
+ball.dx = 2
+ball.dy = 2
 
 
 # Functions to move paddles
@@ -48,12 +51,31 @@ def paddle_a_down():
     y -= 20
     paddle_a.sety(y)
 
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
 # Keyboard binding
 wnd.listen()
 wnd.onkeypress(paddle_a_up, 'w')
 wnd.onkeypress(paddle_a_down, 's')
+wnd.onkeypress(paddle_b_up, 'Up')
+wnd.onkeypress(paddle_b_down, 'Down')
 
 
 #Main Game Loop
 while True:
     wnd.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
